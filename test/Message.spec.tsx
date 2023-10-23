82fd48fe15565880bb1c14e3d42251085b65e3a4
Dmitrii Ostasevich <d.ostasevich@cognigy.com>
2023-10-23 13:17:02 +0200

    add vitest jsdom, test placeholder

---
+++ test/Message.spec.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import { it, describe } from 'vitest'
+import Message from '../src/Message';
+
+
+describe('Message', () => {
+  it('renders text message', () => {
+    const message = { text: 'Hello World'};
+
+    render(<Message source='bot' content={message} />);
+
+    screen.debug();
+
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
-import typedCssModulesPlugin from "vite-plugin-typed-css-modules";
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+// import typedCssModulesPlugin from "vite-plugin-typed-css-modules";
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), cssInjectedByJsPlugin(),typedCssModulesPlugin()],
+  plugins: [react(), cssInjectedByJsPlugin()],
+  // plugins: [react(), cssInjectedByJsPlugin(), typedCssModulesPlugin()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./test/setup.js",
+    // reporters: ["default", "html"],
+  },
   build: {
+    target: "modules", // = es2020 edge88 firefox78 chrome87 safari14 (vite 4.4.5)
+    sourcemap: true,
+    minify: false,
     lib: {
-      entry: 'src/index.ts',
-      name: 'chat-components', 
-      fileName: "message",
-      formats: ['es'],
+      entry: "src/index.ts",
+      name: "chat-components",
+      formats: ["es"],
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"],
       output: {
-        format: 'es',
+        format: "es",
       },
     },
   },
-})
+});
