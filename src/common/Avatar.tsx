2684e343fef0b36a181f917574b32aa2b7d44b91
Dmitrii Ostasevich <d.ostasevich@cognigy.com>
2023-10-23 13:14:07 +0200

    add common components placeholders

---
+++ src/common/Avatar.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from "react";
+import classes from "./Avatar.module.css";
+import classnames from "classnames";
+
+interface IAvatarProps {
+    alt: string;
+    className?: string;
+    children: React.ReactNode;
+    src: string;
+}
+
+const Avatar: FC<IAvatarProps> = props => {
+    const classNames = classnames(classes.bubble, props.className, "avatar");
+    
+    return <img alt={props.alt} className={classNames} src={props.src} />
+};
+
+export default Avatar;
--- src/common/ChatBubble.tsx
+++ src/common/ChatBubble.tsx
@@ -1,13 +1,16 @@
 import React, { FC } from "react";
 import classes from "./ChatBubble.module.css";
+import classnames from "classnames";
 
 interface IChatBubbleProps {
+    className?: string;
     children: React.ReactNode;
 }
 
 const ChatBubble: FC<IChatBubbleProps> = props => {
+    const classNames = classnames(classes.bubble, props.className, "chat-bubble");
     
-    return <div className={classes.bubble}>{props.children}</div>;
+    return <div className={classNames}>{props.children}</div>;
 };
 
 export default ChatBubble;
