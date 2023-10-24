cf106aed2580fa47eb6b2b59b8d1059b80c0e5e9
Dmitrii Ostasevich <d.ostasevich@cognigy.com>
2023-10-24 23:26:51 +0200

    update deps, remove unused package

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-// import typedCssModulesPlugin from "vite-plugin-typed-css-modules";
 
 export default defineConfig({
   plugins: [react(), cssInjectedByJsPlugin()],
-  // plugins: [react(), cssInjectedByJsPlugin(), typedCssModulesPlugin()],
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: "./test/setup.js",
-    // reporters: ["default", "html"],
   },
   build: {
     target: "modules", // = es2020 edge88 firefox78 chrome87 safari14 (vite 4.4.5)
@@ -22,7 +19,7 @@ export default defineConfig({
       formats: ["es"],
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      // external: ["react", "react-dom"], // We will ship all deps 
       output: {
         format: "es",
       },
