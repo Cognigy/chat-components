2b29ba5ca8d058675b08d9df120fd69695323f8a
Dmitrii Ostasevich <d.ostasevich@cognigy.com>
2023-10-27 14:52:38 +0200

    add alert as action for debug, style buttons

--- src/demo.tsx
+++ src/demo.tsx
@@ -25,60 +25,62 @@ const messages: MessageProps[] = [
 	},
 	{
 		message: {
-			"text": null,
-			"data": {
-				"_cognigy": {
-					"_webchat": {
-						"message": {
-							"attachment": {
-								"type": "template",
-								"payload": {
-									"text": "foobar005",
-									"template_type": "button",
-									"buttons": [
+			text: null,
+			data: {
+				_cognigy: {
+					_webchat: {
+						message: {
+							attachment: {
+								type: "template",
+								payload: {
+									text: "Text with buttons template. Here goes the text.\n\nThe text can also be multiline with <strong>HTML</strong> content",
+									template_type: "button",
+									buttons: [
 										{
-											"type": "postback",
-											"payload": "foobar005b1pb",
-											"title": "foobar005b1",
-											"url": "",
-											"webview_height_ratio": "full",
-											"messenger_extensions": false
+											type: "postback",
+											payload: "foobar005b1pb",
+											title: "foobar005b1",
+											url: "",
+											webview_height_ratio: "full",
+											messenger_extensions: false,
 										},
 										{
-											"type": "web_url",
-											"title": "foobar005b2",
-											"url": "foobar005b1url1",
-											"messenger_extensions": false,
-											"webview_height_ratio": "full"
+											type: "web_url",
+											title: "foobar005b2",
+											url: "foobar005b1url1",
+											messenger_extensions: false,
+											webview_height_ratio: "full",
 										},
 										{
-											"type": "postback",
-											"payload": "",
-											"title": "foobar005b3",
-											"url": "",
-											"webview_height_ratio": "full",
-											"messenger_extensions": false
+											type: "postback",
+											payload: "",
+											title: "foobar005b3",
+											url: "",
+											webview_height_ratio: "full",
+											messenger_extensions: false,
 										},
 										{
-											"title": "foobar005b4",
-											"type": "phone_number",
-											"payload": "000111222"
-										}
-									]
-								}
-							}
-						}
-					}
-				}
-			}
-		}
+											title: "foobar005b4",
+											type: "phone_number",
+											payload: "000111222",
+										},
+									],
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	},
 ];
+
+const action = (payload: Record<string, unknown>) => alert(JSON.stringify(payload, null, 2));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		{messages.map((message, index) => (
-			<Message key={index} {...message} />
+			<Message key={index} {...message} action={action} />
 		))}
 	</React.StrictMode>,
 );
--- src/messages/TextWithButtons/TextWithButtons.tsx
+++ src/messages/TextWithButtons/TextWithButtons.tsx
@@ -1,15 +1,16 @@
 import { FC } from "react";
 import { MessagePasstroughProps } from "../types";
+import classes from "./TextWithButtons.module.css";
 
 const TextWithButtons: FC<MessagePasstroughProps> = props => {
     const text = props.message.data._cognigy._webchat.message.attachment.payload.text;
     const buttons = props.message.data._cognigy._webchat.message.attachment.payload.buttons;
 
     const buttonElements = buttons.map((button: any) => {
-        return <button key={button.title} onClick={() => props.action?.("test")}>{button.title}</button>
+        return <button className={classes.button} key={button.title} onClick={() => props.action?.("test")}>{button.title}</button>
     });
 
-    return <><div>{text}</div><div>{buttonElements}</div></> ;
+    return <><div className={classes.text} dangerouslySetInnerHTML={{ __html: text}} /><div className={classes.buttons}>{buttonElements}</div></> ;
 }
 
 export default TextWithButtons;
