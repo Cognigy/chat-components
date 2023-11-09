4b5aed154637ac6a40f9e7c80fbf9c308ea9b555
Fabio Colajanni <f.colajanni@cognigy.com>
2023-11-09 11:30:28 +0100

    preparing list data

--- src/messages/List/List.tsx
+++ src/messages/List/List.tsx
@@ -1,11 +1,45 @@
-import { FC } from "react";
+import { FC, useEffect, useMemo } from "react";
 import { MessagePasstroughProps } from "../types";
 
 const List: FC<MessagePasstroughProps> = props => {
-    console.log(props);
+	const { payload } = props?.message?.data?._cognigy?._webchat?.message?.attachment || {};
+
+	const { elements, top_element_style, buttons } = payload;
+
+	// We support the "large" string to maintain compatibility with old format
+    const showTopElementLarge = top_element_style === "large" || top_element_style === true;
+    
+	const regularElements = showTopElementLarge ? elements.slice(1) : elements;
+	const headerElement = showTopElementLarge ? elements[0] : null;
+	const button = buttons && buttons[0];
+	const listTemplateId = useMemo(() => `webchatListTemplateRoot-${Date.now()}`, []);
+
+    console.log('regularElements', regularElements);
+    console.log('headerElement', headerElement);
+    console.log('button', button);
+
+	useEffect(() => {
+		const chatHistory = document.getElementById("webchatChatHistoryWrapperLiveLogPanel");
+
+		// if (!config?.settings.enableAutoFocus) return;
+
+		if (!chatHistory?.contains(document.activeElement)) return;
+
+		const listTemplateRoot = document.getElementById(listTemplateId);
+		// get the first focusable element within the list and add focus
+		const focusable = listTemplateRoot?.querySelectorAll(
+			'button, [href], [tabindex]:not([tabindex="-1"])',
+		);
+		const firstFocusable = focusable && (focusable[0] as HTMLElement);
+		setTimeout(() => {
+			firstFocusable?.focus();
+		}, 200);
+	}, [listTemplateId]);
 
 	return (
-		<div>List</div>
+		<div className="webchat-list-template-root" role="list" id={listTemplateId}>
+			List
+		</div>
 	);
 };
 
