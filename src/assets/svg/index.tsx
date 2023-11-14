ae9428b31df2573da31b80e3459103998a2c71b1
Fabio Colajanni <f.colajanni@cognigy.com>
2023-11-14 13:43:35 +0100

    refining video component

--- src/assets/svg/index.tsx
+++ src/assets/svg/index.tsx
@@ -1,8 +1,8 @@
 import CloseIcon from "./baseline-close-24px.svg?react";
 import DownloadIcon from "./baseline-download-24px.svg?react";
-import VideoPlayIcon from "./video-play-button.svg?react";
-import PlayIcon from "./play_sharp_icon.svg?react";
+import PlayIcon from "./play_icon.svg?react";
 import PauseIcon from "./pause_icon.svg?react";
+import VideoPlayIcon from "./youtube_icon.svg?react";
 import LinkIcon from "./link.svg?react";
 
 export { CloseIcon, DownloadIcon, VideoPlayIcon, PlayIcon, PauseIcon, LinkIcon };
