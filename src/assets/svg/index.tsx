f85486a1053dcd295a1baef5a8d9babe6b1f7538
Fabio Colajanni <f.colajanni@cognigy.com>
2023-11-23 14:09:05 +0100

    implementing the correct avatar placeholder

--- src/assets/svg/index.tsx
+++ src/assets/svg/index.tsx
@@ -7,10 +7,12 @@ import VideoPlayIcon from "./video_play_white.svg?react";
 import LinkIcon from "./link.svg?react";
 import HeaderEllipsis from "./header_ellipsis.svg?react";
 import ArrowBack from "./arrow_back.svg?react";
-import BotAvatar from "./bot.svg?react";
+import BotAvatar from "./avatar_bot.svg?react";
+import PlaceholderAvatar from "./avatar_placeholder.svg?react";
 
 export {
 	BotAvatar,
+	PlaceholderAvatar,
 	CloseIcon,
 	DownloadIcon,
 	VideoPlayIconBlack,
