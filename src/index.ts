c94ecc25c5e54e9bed3d8659bdd6f6af58d0b852
Dmitrii Ostasevich <d.ostasevich@cognigy.com>
2023-12-07 00:35:29 +0100

    implement typing indicator

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import Message, { MessageProps } from "./messages/Message";
-import { match } from "./matcher";
-import Typography from "./common/Typography/Typography";
 import "@fontsource/figtree/400.css";
 import "@fontsource/figtree/400-italic.css";
 import "@fontsource/figtree/500.css";
@@ -10,6 +7,11 @@ import "@fontsource/figtree/600-italic.css";
 import "@fontsource/figtree/700.css";
 import "@fontsource/figtree/700-italic.css";
 
-export { Message, match };
+import Message, { MessageProps } from "./messages/Message";
+import { match } from "./matcher";
+import Typography from "./common/Typography/Typography";
+import TypingIndicator from "./common/TypingIndicator";
+
+export { Message, match, TypingIndicator };
 export type { MessageProps };
 export { Typography };
