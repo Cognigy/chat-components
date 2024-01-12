ccd3bcf222590188fe3d37cbde4ed55ccc591536
Fabio Colajanni <f.colajanni@cognigy.com>
2024-01-12 11:10:03 +0100

    styling select months and plugin fixes

--- src/messages/DatePicker/flatpickr-plugins/customElements.ts
+++ src/messages/DatePicker/flatpickr-plugins/customElements.ts
@@ -71,26 +71,29 @@ function customElements(pluginConfig: Config): Plugin {
 			}
 		}
 
-		function setAlly() {
+		function setTimeAlly() {
 			fp?.calendarContainer?.focus();
 
 			fp?.calendarContainer?.setAttribute("tabIndex", "0");
 			fp?.calendarContainer?.setAttribute("aria-labelledby", "webchatDatePickerHeaderLabel");
 
-			const hourField = fp?.timeContainer?.getElementsByClassName("flatpickr-hour")?.[0];
-			hourField?.setAttribute("tabIndex", "0");
-			const minutesField = fp?.timeContainer?.getElementsByClassName("flatpickr-minute")?.[0];
-			minutesField?.setAttribute("tabIndex", "0");
-			const amPmField = fp?.timeContainer?.getElementsByClassName("flatpickr-am-pm")?.[0];
-			amPmField?.setAttribute("tabIndex", "0");
-			amPmField?.setAttribute("role", "button");
+			if (!fp?.config?.enableTime) {
+				const hourField = fp?.timeContainer?.getElementsByClassName("flatpickr-hour")?.[0];
+				hourField?.setAttribute("tabIndex", "0");
+				const minutesField =
+					fp?.timeContainer?.getElementsByClassName("flatpickr-minute")?.[0];
+				minutesField?.setAttribute("tabIndex", "0");
+				const amPmField = fp?.timeContainer?.getElementsByClassName("flatpickr-am-pm")?.[0];
+				amPmField?.setAttribute("tabIndex", "0");
+				amPmField?.setAttribute("role", "button");
+			}
 		}
 
 		return {
 			onReady: [
 				upsertTimeArrows,
 				buildTimeArrows,
-				setAlly,
+				setTimeAlly,
 				() => {
 					fp?.loadedPlugins?.push("customElements");
 				},
