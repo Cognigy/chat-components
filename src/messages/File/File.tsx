2a5229c4085d3c07cc71a5c14972d00b44c0f7b3
Sushmitha Sekar <sushmithasekar28@gmail.com>
2024-01-26 15:20:22 +0100

    fix file preview styles and add file url

--- src/messages/File/File.tsx
+++ src/messages/File/File.tsx
@@ -26,36 +26,45 @@ const File: FC = () => {
 	return (
 		<div className={classnames(classes.filesWrapper, "webchat-media-files-template-root")}>
 			{attachments.map((attachment: IUploadFileMetaData, index: number) => {
-				const { fileName, size } = attachment;
+				const { fileName, size, url } = attachment;
 
 				return (
-					<div
-						className={classnames(classes.filePreview, "webchat-media-template-file")}
-						data-testid="file-message"
-						key={index}
-					>
-						<div className={classes.fileNameWrapper}>
+					<a href={url} target="_blank" style={{ textDecoration: "none" }}>
+						<div
+							className={classnames(
+								classes.filePreview,
+								"webchat-media-template-file",
+							)}
+							data-testid="file-message"
+							key={index}
+						>
+							<div className={classes.fileNameWrapper}>
+								<Typography
+									component="span"
+									variant="title2-regular"
+									className={classes.fileName}
+								>
+									{getFileName(fileName)}
+								</Typography>
+								<Typography
+									component="span"
+									variant="title2-regular"
+									className={classes.fileExtension}
+								>
+									{getFileExtension(fileName)}
+								</Typography>
+							</div>
 							<Typography
 								component="span"
 								variant="title2-regular"
-								className={classes.fileName}
+								className={classes.fileSize}
 							>
-								{getFileName(fileName)}
-							</Typography>
-							<Typography
-								component="span"
-								variant="title2-regular"
-								className={classes.fileExtension}
-							>
-								{getFileExtension(fileName)}
+								{size > 1000000
+									? `${(size / 1000000).toFixed(2)} MB`
+									: `${(size / 1000).toFixed(2)} KB`}
 							</Typography>
 						</div>
-						<Typography component="span" variant="title2-regular">
-							{size > 1000000
-								? `${(size / 1000000).toFixed(2)} MB`
-								: `${(size / 1000).toFixed(2)} KB`}
-						</Typography>
-					</div>
+					</a>
 				);
 			})}
 		</div>
