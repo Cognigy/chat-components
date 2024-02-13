55c4660069ee1f05cd8b29c36cbdf3ecb19d2160
Dmitrii Ostasevich <90881+kwinto@users.noreply.github.com>
2024-02-13 16:06:54 +0100

    use interface instead of any

--- src/messages/xApp/XAppSubmitMessage.tsx
+++ src/messages/xApp/XAppSubmitMessage.tsx
@@ -1,12 +1,14 @@
 import { ChatEvent } from "src/index";
 import { useMessageContext } from "../hooks";
 
+import type { IPluginXAppSubmit } from "@cognigy/socket-client/lib/interfaces/messageData";
+
 import classes from "./XApp.module.css";
 
 const XAppSubmitMessage = () => {
 	const { message } = useMessageContext();
 
-	const { success } = (message?.data as any)?._plugin?.data || {};
+	const { success } = (message?.data?._plugin as IPluginXAppSubmit)?.data || {};
 	const text = success ? "Submitted successfully" : "Submission failed";
 
 	return <ChatEvent text={text} className={classes.pill} />;
