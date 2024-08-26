d1695f5b3f659695476e2bed1ec87c36f563c238
Dmitrii Ostasevich <90881+kwinto@users.noreply.github.com>
2024-08-26 16:06:58 +0200

    add ssl to demo page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import svgr from "vite-plugin-svgr";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import GithubActionsReporter from "vitest-github-actions-reporter";
 import dts from "vite-plugin-dts";
+import mkcert from'vite-plugin-mkcert'
+
 
 export default defineConfig({
 	plugins: [
@@ -14,6 +16,7 @@ export default defineConfig({
 			insertTypesEntry: true,
 			include: ["src"],
 		}),
+		mkcert(),
 	],
 	test: {
 		environment: "jsdom",
