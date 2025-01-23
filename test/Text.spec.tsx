119777a6f700998ceb322e010514a6e61d23b73b
vj-venkatesan <v.venkatesan@cognigy.com>
2025-01-23 16:06:57 +0100

    fixup! fix: handle linking scenarios such as query params and deeplinks

--- test/Text.spec.tsx
+++ test/Text.spec.tsx
@@ -7,8 +7,8 @@ describe("Text Component", () => {
 		"https://de.wikipedia.org/wiki/Düsseldorf",
 		"https://de.wikipedia.org/wiki/Überlingen",
 		"https://de.wikipedia.org/wiki/Äpfel",
-		"https://de.wikipedia.org/wiki/Österreich"
-	])("renders %s with special characters in single tag", (testString) => {
+		"https://de.wikipedia.org/wiki/Österreich",
+	])("renders %s with special characters in single tag", testString => {
 		render(<Message message={{ text: testString }} />);
 		const textElement = screen.getByText(testString);
 		expect(textElement).toBeInTheDocument();
@@ -20,19 +20,19 @@ describe("Text Component", () => {
 		["https://example.com/search?q=Äpfel"],
 		["https://example.com/Düsseldorf?q=Österreich&lang=de"],
 		["https://example.com/auth?token=123e4567-e89b-12d3-a456-426614174000"],
-		["https://example.com?token=123e4567-e89b-12d3-a456-426614174000"]
-	])("renders %s with query parameters in single tag", (testString) => {
+		["https://example.com?token=123e4567-e89b-12d3-a456-426614174000"],
+	])("renders %s with query parameters in single tag", testString => {
 		render(<Message message={{ text: testString }} />);
 		const textElement = screen.getByText(testString);
 		expect(textElement).toBeInTheDocument();
 	});
 
-	test.each([
-		"https://example.com/path#section1",
-		"https://example.com/path#Düsseldorf"
-	])("renders %s with deeplinks in single tag", (testString) => {
-		render(<Message message={{ text: testString }} />);
-		const textElement = screen.getByText(testString);
-		expect(textElement).toBeInTheDocument();
-	});
+	test.each(["https://example.com/path#section1", "https://example.com/path#Düsseldorf"])(
+		"renders %s with deeplinks in single tag",
+		testString => {
+			render(<Message message={{ text: testString }} />);
+			const textElement = screen.getByText(testString);
+			expect(textElement).toBeInTheDocument();
+		},
+	);
 });
