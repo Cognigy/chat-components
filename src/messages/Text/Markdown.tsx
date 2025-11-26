75048f12d185ee147ce4a3e84ce1a9875fe3033d
Sushmitha Sekar <sushmithasekar28@gmail.com>
2025-11-26 15:05:22 +0100

    Fix formatting

--- src/messages/Text/Markdown.tsx
+++ src/messages/Text/Markdown.tsx
@@ -1,12 +1,12 @@
-import Markdown from 'react-markdown';
-import rehypeRaw from 'rehype-raw';
-import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
-import remarkGfm from 'remark-gfm';
+import Markdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import rehypeSanitize, { defaultSchema } from "rehype-sanitize";
+import remarkGfm from "remark-gfm";
 
 interface MarkdownComponentProps {
-    processedContent?: string;
-    displayedText?: string;
-    className?: string;
+	processedContent?: string;
+	displayedText?: string;
+	className?: string;
 }
 
 const schema = {
@@ -25,25 +25,23 @@ const schema = {
 	},
 };
 
-export const MarkdownComponent: React.FC<MarkdownComponentProps> = (props) => {
-    const { processedContent, displayedText, className } = props;
+export const MarkdownComponent: React.FC<MarkdownComponentProps> = props => {
+	const { processedContent, displayedText, className } = props;
 
-    return (
-        <Markdown
-            className={className}
-            rehypePlugins={[rehypeRaw, [rehypeSanitize, schema]]}
-            remarkPlugins={[remarkGfm]}
-            urlTransform={url => url}
-            components={{
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                a: ({ node, ...props }) => (
-                    <a target="_blank" rel="noreferrer" {...props} />
-                ),
-            }}
-        >
-            {processedContent || displayedText}
-        </Markdown>
-    );
+	return (
+		<Markdown
+			className={className}
+			rehypePlugins={[rehypeRaw, [rehypeSanitize, schema]]}
+			remarkPlugins={[remarkGfm]}
+			urlTransform={url => url}
+			components={{
+				// eslint-disable-next-line @typescript-eslint/no-unused-vars
+				a: ({ node, ...props }) => <a target="_blank" rel="noreferrer" {...props} />,
+			}}
+		>
+			{processedContent || displayedText}
+		</Markdown>
+	);
 };
 
 export default MarkdownComponent;
