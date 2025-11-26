7889404e1c3ad3462967f62a92443e3433b88821
Sushmitha Sekar <sushmithasekar28@gmail.com>
2025-11-26 15:07:35 +0100

    Remove sanitization and schema for markdown

--- src/messages/Text/Markdown.tsx
+++ src/messages/Text/Markdown.tsx
@@ -1,6 +1,5 @@
 import Markdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
-import rehypeSanitize, { defaultSchema } from "rehype-sanitize";
 import remarkGfm from "remark-gfm";
 
 interface MarkdownComponentProps {
@@ -9,21 +8,6 @@ interface MarkdownComponentProps {
 	className?: string;
 }
 
-const schema = {
-	...defaultSchema,
-	attributes: {
-		...defaultSchema.attributes,
-		a: [
-			...(defaultSchema.attributes?.a || []),
-			["href", /.*/], // allow any href
-			["style", /.*/],
-		],
-	},
-	protocols: {
-		...defaultSchema.protocols,
-		href: [...(defaultSchema.protocols?.href || []), "tel"],
-	},
-};
 
 export const MarkdownComponent: React.FC<MarkdownComponentProps> = props => {
 	const { processedContent, displayedText, className } = props;
@@ -31,7 +15,7 @@ export const MarkdownComponent: React.FC<MarkdownComponentProps> = props => {
 	return (
 		<Markdown
 			className={className}
-			rehypePlugins={[rehypeRaw, [rehypeSanitize, schema]]}
+			rehypePlugins={[rehypeRaw]}
 			remarkPlugins={[remarkGfm]}
 			urlTransform={url => url}
 			components={{
